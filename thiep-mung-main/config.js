7402d7d70b32a1940b08b3d52591bc5fb5c790af
phuctruong03qt <phuctruong03qt@gmail.com>
2023-05-31 17:55:12 +0700

    init file

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Yêu';
+const giftUrl = 'https://www.facebook.com/linh.vuthuy.733076';
+const eventName = '1-6 vui vẻ nhé bé';
+const titleCard = 'Tặng bé của anh';
+const contentCard = 'Chúc bé của anh 1/6 tràn ngập niềm vui và hạnh phúc nhé. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Anh mãi yêu bé ❤❤❤';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'be.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
